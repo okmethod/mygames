fe9770167ab2577fe1fe89223cf5a45029a1b8c4
okmethod <83124702+okmethod@users.noreply.github.com>
2021-04-29 13:15:42 +0900

    version 2.0.1 class method access control

--- reversi.py
+++ reversi.py
@@ -27,84 +27,86 @@ sound_end   = pygame.mixer.Sound('media\Windows Error.wav')
 ## クラス：リバーシのゲーム状態を管理する
 ################################################################
 class GameStateReversi:
-	#### クラス定数
+	##public## クラス定数
 	EMPTY = -1
 	BLACK = 0
 	WHITE = 1
 	
-	#### コンストラクタ：ゲーム状態を初期化する
+	##private## コンストラクタ：ゲーム状態を初期化する
 	def __init__(self, board_size):
-		self.board_len_y = board_size
-		self.board_len_x = board_size
-		self.board_state = [[self.EMPTY for pos_x in range(self.board_len_x)] for pos_y in range(self.board_len_y)]
-		self.player_data  = [{}, {}]
-		self.player_data[self.BLACK] = {'player_name' : 'Black', 'theme_color' : (  0,   0,   0), 'stone_count' : 0 }
-		self.player_data[self.WHITE] = {'player_name' : 'White', 'theme_color' : (255, 255, 255), 'stone_count' : 0 }
-		self.active_player = self.BLACK
-		self.winner_player = None
-		self.pass_flg = False
-		self.end_flg  = False
-	
-	#### 管理コマンド：初期配置を設定する
-	def init_board(self):
-		self.board_state[self.board_len_y//2][self.board_len_x//2]     = self.WHITE
-		self.board_state[self.board_len_y//2][self.board_len_x//2-1]   = self.BLACK
-		self.board_state[self.board_len_y//2-1][self.board_len_x//2]   = self.BLACK
-		self.board_state[self.board_len_y//2-1][self.board_len_x//2-1] = self.WHITE
-		self.player_data[self.BLACK]['stone_count'] = 2
-		self.player_data[self.WHITE]['stone_count'] = 2
+		self.__board_len_y = board_size
+		self.__board_len_x = board_size
+		self.__board_state = [[self.EMPTY for pos_x in range(self.__board_len_x)] for pos_y in range(self.__board_len_y)]
+		self.__player_data  = [{}, {}]
+		self.__player_data[self.BLACK] = {'player_name' : 'Black', 'theme_color' : (  0,   0,   0), 'stone_count' : 0 }
+		self.__player_data[self.WHITE] = {'player_name' : 'White', 'theme_color' : (255, 255, 255), 'stone_count' : 0 }
+		self.__active_player = self.BLACK
+		self.__winner_player = None
+		self.__pass_flg = False
+		self.__end_flg  = False
+		# 初期配置の石を設置
+		self.__init_board()
 		
-	#### 管理コマンド：反対の色を取得する
+	##public## getter：反対の色を取得する
 	def get_reverse_color(self, c):
 		if c == self.BLACK:
 			return self.WHITE
 		if c == self.WHITE:
 			return self.BLACK
 	
-	#### 管理コマンド：アクティブプレイヤーを取得する
+	##public## getter：アクティブプレイヤーを取得する
 	def get_active_player(self):
-		return self.active_player
+		return self.__active_player
 	
-	#### 管理コマンド：プレイヤー名を取得する
+	##public## getter：勝利プレイヤーを取得する
+	def get_winner_player(self):
+		return self.__winner_player
+	
+	##public## getter：プレイヤー名を取得する
 	def get_player_name(self, c):
-		return self.player_data[c]['player_name']
+		return self.__player_data[c]['player_name']
 	
-	#### 管理コマンド：テーマカラーを取得する
+	##public## getter：テーマカラーを取得する
 	def get_theme_color(self, c):
-		return self.player_data[c]['theme_color']
+		return self.__player_data[c]['theme_color']
 	
-	#### 管理コマンド：石数を取得する
+	##public## getter：石数を取得する
 	def get_stone_count(self, c):
-		return self.player_data[c]['stone_count']
+		return self.__player_data[c]['stone_count']
 	
-	#### 管理コマンド：指定位置のタイルの状態を取得する
+	##public## getter：指定位置のタイルの状態を取得する
 	def get_board_state(self, pos):
 		pos_x, pos_y  = pos[0], pos[1]
-		return self.board_state[pos_y][pos_x]
+		return self.__board_state[pos_y][pos_x]
 	
-	#### 管理コマンド：勝利プレイヤーを判定する
-	def decide_winner_player(self):
-		if self.get_stone_count(self.BLACK) > self.get_stone_count(self.WHITE):
-			self.winner_player = self.BLACK
-		elif self.get_stone_count(self.BLACK) < self.get_stone_count(self.WHITE):
-			self.winner_player = self.WHITE
-		else:
-			self.winner_player = None
-	
-	#### 管理コマンド：勝利プレイヤーを取得する
-	def get_winner_player(self):
-		return self.winner_player
-	
-	#### 管理コマンド：ゲームが終了しているかどうかを確認する
+	##public## getter：ゲームが終了しているかどうかを確認する
 	def get_end_flg(self):
-		return self.end_flg
+		return self.__end_flg
 	
-	#### 管理コマンド：アクティブプレイヤーを交代する
-	def change_turn(self):
-		self.active_player = self.get_reverse_color(self.active_player)
+	##private## 管理コマンド：初期配置を設定する
+	def __init_board(self):
+		self.__board_state[self.__board_len_y//2][self.__board_len_x//2]     = self.WHITE
+		self.__board_state[self.__board_len_y//2][self.__board_len_x//2-1]   = self.BLACK
+		self.__board_state[self.__board_len_y//2-1][self.__board_len_x//2]   = self.BLACK
+		self.__board_state[self.__board_len_y//2-1][self.__board_len_x//2-1] = self.WHITE
+		self.__player_data[self.BLACK]['stone_count'] = 2
+		self.__player_data[self.WHITE]['stone_count'] = 2
 	
-	#### 管理コマンド：石を反転する(またはシミュレーションする)
-	def reverse_stone(self, pos, dir, update_flg):
+	##private## 管理コマンド：アクティブプレイヤーを交代する
+	def __change_turn(self):
+		self.__active_player = self.get_reverse_color(self.__active_player)
+	
+	##private## 管理コマンド：勝利プレイヤーを判定する
+	def __decide_winner_player(self):
+		if self.get_stone_count(self.BLACK) > self.get_stone_count(self.WHITE):
+			self.__winner_player = self.BLACK
+		elif self.get_stone_count(self.BLACK) < self.get_stone_count(self.WHITE):
+			self.__winner_player = self.WHITE
+		else:
+			self.__winner_player = None
+	
+	##private## 管理コマンド：石を反転する(またはシミュレーションする)
+	def __reverse_stone(self, pos, dir, update_flg):
 		pos_x, pos_y  = pos[0], pos[1]
 		dir_x, dir_y  = dir[0], dir[1]
 		
@@ -112,106 +114,106 @@ class GameStateReversi:
 		reverse_pos_list = []
 		
 		# はみ出さない限り、繰り返す
-		while (pos_x + dir_x >= 0) and (pos_x + dir_x <= self.board_len_x-1) and \
-			  (pos_y + dir_y >= 0) and (pos_y + dir_y <= self.board_len_y-1):
+		while (pos_x + dir_x >= 0) and (pos_x + dir_x <= self.__board_len_x-1) and \
+			  (pos_y + dir_y >= 0) and (pos_y + dir_y <= self.__board_len_y-1):
 			
 			# 指定された方向の隣の位置
 			pos_x = pos_x + dir_x
 			pos_y = pos_y + dir_y
 			
 			# 空白の場合
-			if self.board_state[pos_y][pos_x] == self.EMPTY:
+			if self.__board_state[pos_y][pos_x] == self.EMPTY:
 				break
 			# 同じ色でない場合
-			elif self.board_state[pos_y][pos_x] != self.active_player:
+			elif self.__board_state[pos_y][pos_x] != self.__active_player:
 				# 反転位置を予約する
 				reverse_pos_list.append([pos_x, pos_y])
 			# 同じ色の場合
-			elif self.board_state[pos_y][pos_x] == self.active_player:
+			elif self.__board_state[pos_y][pos_x] == self.__active_player:
 				# 1つ以上の反転位置が予約されている場合
 				if len(reverse_pos_list) > 0:
 					# 更新フラグがオンの場合
 					if update_flg == True:
 						for pos in reverse_pos_list:
 							# 反転位置を更新する
-							self.board_state[pos[1]][pos[0]] = self.active_player
+							self.__board_state[pos[1]][pos[0]] = self.__active_player
 							# 石カウンタを更新する
-							self.player_data[self.active_player]['stone_count'] += 1
-							self.player_data[self.get_reverse_color(self.active_player)]['stone_count'] -= 1
+							self.__player_data[self.__active_player]['stone_count'] += 1
+							self.__player_data[self.get_reverse_color(self.__active_player)]['stone_count'] -= 1
 					return True
 		
 		return False
 	
-	#### 管理コマンド：指定位置が有効手かどうかを確認する
-	def validate_set_pos(self, pos):
+	##private## 管理コマンド：指定位置が有効手かどうかを判定する
+	def __validate_set_pos(self, pos):
 		pos_x, pos_y = pos[0], pos[1]
 		
 		# 指定位置がボード内の座標かどうかを確認する
-		if (pos_y < 0) and (self.board_len_y <= pos_y) and \
-		   (pos_x < 0) and (self.board_len_x <= pos_x):
-			# アクションの結果を返却する
+		if (pos_y < 0) and (self.__board_len_y <= pos_y) and \
+		   (pos_x < 0) and (self.__board_len_x <= pos_x):
+			# 判定結果を返却する
 			description_str = 'The specified position is out of range.'
 			return {'is_valid' : False, 'description' : description_str}
 		
 		# 指定位置が空タイルかどうかを確認する
-		if self.board_state[pos_y][pos_x] != self.EMPTY:
-			# アクションの結果を返却する
+		if self.__board_state[pos_y][pos_x] != self.EMPTY:
+			# 判定結果を返却する
 			description_str = 'The specified position is not empty.'
 			return {'is_valid' : False, 'description' : description_str}
 		
 		# 石が反転するかどうかを確認する
-		if self.reverse_stone([pos_x, pos_y], [ 0, -1], False) or \
-		   self.reverse_stone([pos_x, pos_y], [ 1, -1], False) or \
-		   self.reverse_stone([pos_x, pos_y], [ 1,  0], False) or \
-		   self.reverse_stone([pos_x, pos_y], [ 1,  1], False) or \
-		   self.reverse_stone([pos_x, pos_y], [ 0,  1], False) or \
-		   self.reverse_stone([pos_x, pos_y], [-1,  1], False) or \
-		   self.reverse_stone([pos_x, pos_y], [-1,  0], False) or \
-		   self.reverse_stone([pos_x, pos_y], [-1, -1], False):
-			# アクションの結果を返却する
+		if self.__reverse_stone([pos_x, pos_y], [ 0, -1], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [ 1, -1], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [ 1,  0], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [ 1,  1], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [ 0,  1], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [-1,  1], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [-1,  0], False) or \
+		   self.__reverse_stone([pos_x, pos_y], [-1, -1], False):
+			# 判定結果を返却する
 			description_str = 'The specified position flips some stones.'
 			return {'is_valid' : True, 'description' : description_str}
 		else:
-			# アクションの結果を返却する
+			# 判定結果を返却する
 			description_str = 'The specified position flips no stones.'
 			return {'is_valid' : False, 'description' : description_str}
 	
-	#### プレイヤーアクション：石を設置する
+	##public## プレイヤーアクション：石を設置する
 	def action_set_stone(self, pos):
 		pos_x, pos_y = pos[0], pos[1]
 		
 		# ゲーム終了フラグがONの場合、何もしない
-		if self.end_flg == True:
+		if self.__end_flg == True:
 			# アクションの結果を返却する
 			description_str = 'This game has terminated.'
 			return {'is_valid' : False, 'description' : description_str}
 		
 		# 指定位置が有効手かどうかを確認する
-		result_dict = self.validate_set_pos(pos)
+		result_dict = self.__validate_set_pos(pos)
 		
 		# 有効手であった場合
 		if result_dict['is_valid']:
 			# 当該タイルをターンプレイヤーの色に変更する
-			self.board_state[pos_y][pos_x] = self.active_player
+			self.__board_state[pos_y][pos_x] = self.__active_player
 			# 石を反転する(8方向)
-			self.reverse_stone([pos_x, pos_y], [ 0, -1], True) # 上
-			self.reverse_stone([pos_x, pos_y], [ 1, -1], True) # 右上
-			self.reverse_stone([pos_x, pos_y], [ 1,  0], True) # 右
-			self.reverse_stone([pos_x, pos_y], [ 1,  1], True) # 右下
-			self.reverse_stone([pos_x, pos_y], [ 0,  1], True) # 下
-			self.reverse_stone([pos_x, pos_y], [-1,  1], True) # 左下
-			self.reverse_stone([pos_x, pos_y], [-1,  0], True) # 左
-			self.reverse_stone([pos_x, pos_y], [-1, -1], True) # 左上
+			self.__reverse_stone([pos_x, pos_y], [ 0, -1], True) # 上
+			self.__reverse_stone([pos_x, pos_y], [ 1, -1], True) # 右上
+			self.__reverse_stone([pos_x, pos_y], [ 1,  0], True) # 右
+			self.__reverse_stone([pos_x, pos_y], [ 1,  1], True) # 右下
+			self.__reverse_stone([pos_x, pos_y], [ 0,  1], True) # 下
+			self.__reverse_stone([pos_x, pos_y], [-1,  1], True) # 左下
+			self.__reverse_stone([pos_x, pos_y], [-1,  0], True) # 左
+			self.__reverse_stone([pos_x, pos_y], [-1, -1], True) # 左上
 			# 石カウンタをインクリメントする
-			self.player_data[self.active_player]['stone_count'] += 1
+			self.__player_data[self.__active_player]['stone_count'] += 1
 			# 空タイルが残っていなければ、勝者を判定してゲーム終了フラグをオンにする
-			if self.get_stone_count(self.BLACK) + self.get_stone_count(self.WHITE) == self.board_len_y * self.board_len_x:
-				self.decide_winner_player()
-				self.end_flg = True
+			if self.get_stone_count(self.BLACK) + self.get_stone_count(self.WHITE) == self.__board_len_y * self.__board_len_x:
+				self.__decide_winner_player()
+				self.__end_flg = True
 			# ターンプレイヤーを交代する
-			self.change_turn()
+			self.__change_turn()
 			# パスフラグをオフにする
-			self.pass_flg = False
+			self.__pass_flg = False
 			# アクションの結果を返却する
 			description_str = 'The specified position flipped some stones.'
 			return {'is_valid' : True, 'description' : description_str}
@@ -219,51 +221,51 @@ class GameStateReversi:
 			# アクションの結果を返却する
 			return {'is_valid' : False, 'description' : result_dict['description']}
 	
-	#### プレイヤーアクション：パスする
+	##public## プレイヤーアクション：パスする
 	def action_pass(self):
 		
 		# ゲーム終了フラグがONの場合、何もしない
-		if self.end_flg == True:
+		if self.__end_flg == True:
 			# アクションの結果を返却する
 			description_str = 'This game has terminated.'
 			return {'is_valid' : False, 'description' : description_str}
 		
 		# 各タイルを確認し、有効手が残っていれば何もしない
-		for pos_x in range(self.board_len_x):
-			for pos_y in range(self.board_len_y):
-				if self.validate_set_pos([pos_x, pos_y])['is_valid']:
+		for pos_x in range(self.__board_len_x):
+			for pos_y in range(self.__board_len_y):
+				if self.__validate_set_pos([pos_x, pos_y])['is_valid']:
 					# アクションの結果を返却する
 					description_str = 'Some valid position are left.'
 					return {'is_valid' : False, 'description' : description_str}
 		
 		# パスが連続している場合
-		if self.pass_flg == True:
+		if self.__pass_flg == True:
 			# 勝者を判定してゲーム終了フラグをオンにする
-			self.decide_winner_player()
-			self.end_flg = True
+			self.__decide_winner_player()
+			self.__end_flg = True
 			# アクションの結果を返却する
 			description_str = 'No valid position are left for any players.'
 			return {'is_valid' : True, 'description' : description_str}
 		else:
 			# パスフラグをオンにし、ターンプレイヤーを交代する
-			self.pass_flg = True
-			self.change_turn()
+			self.__pass_flg = True
+			self.__change_turn()
 			# アクションの結果を返却する
 			description_str = 'Active player is changed.'
 			return {'is_valid' : True, 'description' : description_str}
 	
-	#### プレイヤーアクション：投了する
+	##public## プレイヤーアクション：投了する
 	def action_give_up(self):
 		
 		# ゲーム終了フラグがONの場合、何もしない
-		if self.end_flg == True:
+		if self.__end_flg == True:
 			# アクションの結果を返却する
 			description_str = 'This game has terminated.'
 			return {'is_valid' : False, 'description' : description_str}
 		
 		# 非アクティブプレイヤーを勝者とし、ゲーム終了フラグをオンにする
-		self.winner_player = self.get_reverse_color(self.active_player)
-		self.end_flg = True
+		self.__winner_player = self.get_reverse_color(self.__active_player)
+		self.__end_flg = True
 		
 		# アクションの結果を返却する
 		description_str = 'Active player gave up.'
@@ -297,8 +299,6 @@ def main():
 	
 	# ゲームの状態を保持するオブジェクト
 	game_state = GameStateReversi(BOARD_SIZE)
-	# 初期配置の石を設置
-	game_state.init_board()
 	
 	#### 画面描画ループ
 	while True:
