53001787052dc7c6c77064ad91c0f47f1dac60b3
okmethod <83124702+okmethod@users.noreply.github.com>
2021-05-05 13:07:38 +0900

    rect_dictnolistまたはdict

--- lib/GameBaseClass/ScreenViewBase.py
+++ lib/GameBaseClass/ScreenViewBase.py
@@ -16,7 +16,16 @@ class ScreenViewBase:
 		# 各サーフェイスを保持するディクショナリ
 		self._sfc_dict    = {}
 		for k in rect_dict.keys():
-			self._sfc_dict[k] = pygame.Surface(rect_dict[k].size)
+			if type(rect_dict[k]) is list:
+				self._sfc_dict[k] = []
+				for i, rect in enumerate(rect_dict[k]):
+					self._sfc_dict[k].append(pygame.Surface(rect.size))
+			elif type(rect_dict[k]) is dict:
+				self._sfc_dict[k] = {}
+				for sub_k in rect_dict[k]:
+					self._sfc_dict[k][sub_k] = pygame.Surface(rect_dict[k][sub_k].size)
+			else:
+				self._sfc_dict[k] = pygame.Surface(rect_dict[k].size)
 		# フォントオブジェクト
 		self._smallfont   = pygame.font.SysFont(None, font_size)
 		self._largefont   = pygame.font.SysFont(None, font_size * 2)
@@ -24,7 +33,15 @@ class ScreenViewBase:
 	##private## 内部メソッド：全体画面への各サーフェイスの貼り付け
 	def _blit_main_screen(self):
 		for k in self._sfc_dict.keys():
-			self._main_screen.blit(self._sfc_dict[k], self._rect_dict[k])
+			if type(self._sfc_dict[k]) is list:
+				for i, sfc in enumerate(self._sfc_dict[k]):
+					self._main_screen.blit(sfc, self._rect_dict[k][i])
+			elif type(self._sfc_dict[k]) is dict:
+				for sub_k in self._sfc_dict[k]:
+					self._main_screen.blit(self._sfc_dict[k][sub_k], self._rect_dict[k][sub_k])
+			else:
+				self._main_screen.blit(self._sfc_dict[k], self._rect_dict[k])
+			
 	
 	##public## ゲーム画面を生成する（要オーバーライド）
 	def draw_view(self):
