6d1a7aa3967c3d1d3e90b43875223a58a3c3cd74
okmethod <83124702+okmethod@users.noreply.github.com>
2021-05-13 22:00:58 +0900

    fix conf

--- bin/reversi/reversi_conf.py
+++ bin/reversi/reversi_conf.py
@@ -13,14 +13,14 @@ BUTTON_SIZE_H  = 1	# ボタン縦幅のタイル数
 BUTTON_SIZE_W  = 4	# ボタン横幅のタイル数
 
 # プレイヤー情報の定義
-PLAYER1_NAME  = 'BLACK'
+PLAYER1_NAME  = 'YOU'
 PLAYER1_COLOR = (  0,   0,   0)
-PLAYER2_NAME  = 'CHIHARU'
+PLAYER2_NAME  = 'CHI-CHAN'
 PLAYER2_COLOR = (255, 192, 192)
 
 # 読み込みファイルパス
 WAV_FILENAME_VALID     = 'Windows Navigation Start.wav'
 WAV_FILENAME_INVALID   = 'Windows Critical Stop.wav'
 IMAGE_FILEPATH_PLAYER1 = None # 画像を使用しない場合はNoneを設定
-IMAGE_FILEPATH_PLAYER2 = 'chi-chan-desu.png'
+IMAGE_FILEPATH_PLAYER2 = 'chi-chan.png'
 
